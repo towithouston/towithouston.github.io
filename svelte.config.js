ef8c56f6fd6a01bb386c74efc0b3f999e144beb0
vladimir1284 <vladimir.rdguez@gmail.com>
2023-06-01 10:32:34 -0400

    no jekyll on root

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/towithouston',
+        }
 	}
 };
 

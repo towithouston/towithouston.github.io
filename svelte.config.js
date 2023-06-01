9529be8b4c40074eae654eb5e7a8b77b13308931
vladimir1284 <vladimir.rdguez@gmail.com>
2023-06-01 11:11:27 -0400

    assets path

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
         paths: {
-            base: dev ? '' : '/build',
+            assets: dev ? '' : 'https://towithouston.github.io/build',
         },
 		appDir: 'internal'
 	}
